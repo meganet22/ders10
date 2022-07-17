763fd740aa3dbc104ab0ceac408e679259211ab3
meganet22 <meganet.advances@gmail.com>
2022-07-17 20:09:46 +0300

    button click event

---
+++ src/components/C2.jsx
@@ -0,0 +1,14 @@
+function C2() {
+    function uyari() {
+        alert ("Uyarı!");
+    }
+
+    return (
+        <>
+            <p>C2 Komponenti</p>
+            <button onClick={uyari}>Uyarı</button>
+        </>
+    );
+}
+
+export default C2;
